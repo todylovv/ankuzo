89e716290a69f6e7bec7d1f16eb1436ed408c291
Александр <87193684+todylovv@users.noreply.github.com>
2026-06-02 21:32:07 +0300

    fix: deploy React build to GitHub Pages

--- src/hooks/useMotionFoundation.js
+++ src/hooks/useMotionFoundation.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import SplitType from 'split-type';
